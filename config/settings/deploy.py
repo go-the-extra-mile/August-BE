dafbe86cf234e28d514b726f9634cd8546fbfa39
MyungJune <kevinshin123@gmail.com>
2023-09-30 00:37:31 +0900

    edit deploy db name to be env variable

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -7,13 +7,14 @@ ALLOWED_HOSTS = [
     'ec2-15-164-218-52.ap-northeast-2.compute.amazonaws.com',
 ]
 
+DEPLOY_DB_NAME = get_secret('DEPLOY_DB_NAME')
 DEPLOY_DB_USER = get_secret('DEPLOY_DB_USER')
 DEPLOY_DB_PASSWORD = get_secret('DEPLOY_DB_PASSWORD')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'august-db',
+        'NAME': DEPLOY_DB_NAME,
         'USER': DEPLOY_DB_USER,
         'PASSWORD': DEPLOY_DB_PASSWORD,
         'HOST': 'localhost',
