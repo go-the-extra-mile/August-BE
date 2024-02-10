ed99f7b68f5e6a9d4893e86769f1b36b4f2ce7c0
MyungJune <kevinshin123@gmail.com>
2024-02-11 01:05:21 +0900

    increase token lifetime for testing

--- config/settings/base.py
+++ config/settings/base.py
@@ -197,7 +197,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
 }
