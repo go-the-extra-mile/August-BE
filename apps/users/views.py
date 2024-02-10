3e43544bc2b61260a32a647ab14807f8bd956bf4
MyungJune <kevinshin123@gmail.com>
2024-02-10 16:23:23 +0900

    remove lines for debug

--- apps/users/views.py
+++ apps/users/views.py
@@ -16,7 +16,6 @@ class GoogleLoginView(APIView):
         response_type = "code"
         scope = "profile%20email"
         url = f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}&scope={scope}"
-        print(url)
         return redirect(url)
 
 
@@ -32,10 +31,10 @@ class GoogleLoginCallbackView(SocialLoginView):
             return Response(data={"error": error}, status=status.HTTP_400_BAD_REQUEST)
 
         self.request.data.update({"code": code})
-        print(f"code: {code}")
 
         return self.post(request, args, kwargs)
-    
+
+
 class GoogleLoginCallbackViewMobile(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
     callback_url = ""
