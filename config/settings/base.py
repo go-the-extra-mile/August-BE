bdbc57e85505e7e7ec80a9dfc8255bdd7f0ea887
MyungJune <kevinshin123@gmail.com>
2024-02-11 15:01:36 +0900

    decrease access token lifetime for testing

--- config/settings/base.py
+++ config/settings/base.py
@@ -197,7 +197,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
 }
