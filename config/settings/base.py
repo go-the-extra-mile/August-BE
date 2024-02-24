64d11e9b6523164be24df053f156efc23aba8b06
MyungJune <kevinshin123@gmail.com>
2024-02-25 02:24:18 +0900

    update access token lifetime

--- config/settings/base.py
+++ config/settings/base.py
@@ -198,7 +198,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
 }
