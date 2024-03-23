1153283ee4d55be04480b4cc8c03a3cdedd69d90
MyungJune <kevinshin123@gmail.com>
2024-03-23 23:56:04 +0900

    Remove course_code field from serializer that serializes timetable and wizard response due to redudancy

--- apps/wizard/serializers.py
+++ apps/wizard/serializers.py
@@ -7,7 +7,6 @@ from apps.courses.serializers import InstructorNameTeachSerializer, MeetingSeria
 class OpenedSectionWithCourseNameSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source="section.course.name")
     credits = serializers.IntegerField(source="section.course.credits")
-    course_code = serializers.CharField(source="section.course.course_code")
     section_code = serializers.CharField(source="section.section_code")
     instructors = InstructorNameTeachSerializer(source="teach_set", many=True)
     meetings = MeetingSerializer(source="meeting_set", many=True)
@@ -18,7 +17,6 @@ class OpenedSectionWithCourseNameSerializer(serializers.ModelSerializer):
             "id",
             "name",
             "credits",
-            "course_code",
             "section_code",
             "instructors",
             "meetings",
